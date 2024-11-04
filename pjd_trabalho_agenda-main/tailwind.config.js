76e097642b2890bb4c5435ff36d5db94a37a31df
LarissaFurtadoOnido <olarissafurtado@gmail.com>
2024-11-04 17:36:35 -0300

    up

---
+++ pjd_trabalho_agenda-main/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,ejs}',   
+    './views/**/*.{html,ejs}',
+    './frontend/**/*.{html, js, ejs}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
